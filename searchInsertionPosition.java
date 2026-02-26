d758e48b1d0f695cfe303e1d4c8a2035e8b7e26e
vinaycraj <vinaycraj1122@gmail.com>
2026-02-26 23:31:06 +0530

    5th leetcode problem

---
+++ searchInsertionPosition.java
@@ -0,0 +1,14 @@
+//35 search insertion position
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for (int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i; // if value matches returns i th index
+            }
+            else if(nums[i]>target){ // if ith element is greater than target then returns ith index
+                return i; 
+            }
+        }
+        return nums.length;
+    }
+}
