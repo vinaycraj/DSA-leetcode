388f7045ce7f84cbc65ef4fb0b6d7d1785836d62
vinaycraj <vinaycraj1122@gmail.com>
2026-03-02 22:30:21 +0530

    leetcode problem 11

---
+++ containerWithMostWater.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left=0;
+        int  right=height.length-1;
+        int max=0;
+        while(left<right){
+            int h=Math.min(height[left], height[right]);
+            int w=right-left;
+            int area=h*w;
+            max=Math.max(max, area);
+
+            if(height[left]<height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return max;
+    }
+}
