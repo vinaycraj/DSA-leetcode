dd46c366a5ffd020b9f8ec07f893b7b52b9657d7
vinaycraj <vinaycraj1122@gmail.com>
2026-03-17 22:19:36 +0530

    leetcode 16 solved

---
+++ sum3Closest.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closest=nums[0]+nums[1]+nums[2];
+        for(int i=0;i<nums.length-2;i++){
+            int left=i+1;
+            int right=nums.length-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(Math.abs(target-sum)<Math.abs(target-closest)){ //gives positive and checks if new value diff is smaller than diff btw prev closest and target
+                    closest=sum; // then updates the closest value with new value
+                }
+                if(sum<target){
+                    left++;
+                }else{
+                    right--;
+                }
+            }
+        }return closest;
+    }
+}
