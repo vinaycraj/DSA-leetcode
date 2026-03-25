96f246230cff27b79e76e34357f0bdf26aa7a9c4
vinaycraj <vinaycraj1122@gmail.com>
2026-03-25 23:09:14 +0530

    leetcode running sum problem

---
+++ runningSum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int sum=0;
+        int[] result=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            result[i]=sum;
+        }
+        return result;
+    }
+}
+//Runnning sum of 1Darray
